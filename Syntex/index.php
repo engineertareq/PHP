0bd82de967d44ee1d47e9985cc36abf67ab41450
engineertareq <engineertareqbd@gmail.com>
2025-10-21 11:24:56 +0600

    first php commit

---
+++ Syntex/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Life-er first php file</h2>
+
+    <?php
+    echo "Hello World!<br>"; 
+    print "I can also print the code - echo is my sibling\n";
+    ?>
+</body>
+</html>
